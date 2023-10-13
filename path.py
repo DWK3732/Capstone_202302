ca591118a2dc08e2339e7cb1d9fa3c61c331c9a4
DWK3732 <ugi9297@naver.com>
2023-10-13 20:12:37 +0900

    [modify] changed path.py from 1 file to 7 files

--- path.py
+++ path.py
@@ -1,22 +1,25 @@
 import pandas as pd
 import folium
 
-# 1. csv 파일 읽기
-data = pd.read_csv('predictions_6.csv')
 
-# 2. 필요한 데이터 추출
-lons = data['lons'].values
-lats = data['lats'].values
-times = data['time'].values
+for index in range(7):
 
-# 3. 지도에 데이터 표시하기
-# 초기 지도 위치와 확대 정도 설정 (예시: 첫 번째 데이터를 중심으로)
-m = folium.Map(location=[lats[0], lons[0]], zoom_start=10)
+    # 1. csv 파일 읽기
+    data = pd.read_csv(f'predictions_{index}.csv')
 
-# 각 위치에 마커와 선 추가하기
-for lat, lon, time in zip(lats, lons, times):
-    folium.Marker([lat, lon], tooltip=f"Time: {int(time)} hours since 2016-01-01 00:00").add_to(m)
-folium.PolyLine(list(zip(lats, lons)), color="blue", weight=2.5).add_to(m)
+    # 2. 필요한 데이터 추출
+    lons = data['lons'].values
+    lats = data['lats'].values
+    times = data['time'].values
 
-# 4. html 파일로 저장하기(interactive map)
-m.save('map.html')
+    # 3. 지도에 데이터 표시하기
+    # 초기 지도 위치와 확대 정도 설정 (예시: 첫 번째 데이터를 중심으로)
+    m = folium.Map(location=[lats[0], lons[0]], zoom_start=10)
+
+    # 각 위치에 마커와 선 추가하기
+    for lat, lon, time in zip(lats, lons, times):
+        folium.Marker([lat, lon], tooltip=f"Time: {int(time)} hours since 2016-01-01 00:00").add_to(m)
+    folium.PolyLine(list(zip(lats, lons)), color="blue", weight=2.5).add_to(m)
+
+    # 4. html 파일로 저장하기(interactive map)
+    m.save(f'map_{index}.html')
